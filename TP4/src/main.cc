045007346f53d3af11c94ffea0eea64ed0ba4b95
micalevisk <mllc@icomp.ufam.edu.br>
2017-04-13 09:57:04 -0400

    checa o 'a(ll)'; usando iterator

--- TP4/src/main.cc
+++ TP4/src/main.cc
@@ -34,73 +34,11 @@ int main(int argc, char* argv[])
 	/// Header do CSV
 	cout << "instancia,algoritmo,comparacoes,trocas,tempo\n";//regex: '([0-9]+),([a-zA-Z]+),([^)]+),([^)]+),([^)]+)'
 
-	for(const auto& alg : params.algoritmosUtilizados){
-		if(alg.second)
-			obj.executarAlgoritmo(alg.first);
+	bool runAll = params.algoritmosUtilizados['a'];
+	for(unordered_map<char,bool>::iterator it=++params.algoritmosUtilizados.begin(); it!=params.algoritmosUtilizados.end(); ++it){
+		if(runAll || it->second)
+			obj.executarAlgoritmo(it->first);
 	}
-	/*
-	/// Teste do Selection Sort
-	analiseCorrente = obj.analytics_selectionsort();
-	if(params.algoritmosUtilizados['a'] || params.algoritmosUtilizados[analiseCorrente.nomeAlgoritmo[0]]){
-		//cout << analiseCorrente;
-		#ifdef VERBOSE
-			cout << "[depois do selection]: ";
-			obj.printDados();
-			cout << endl;
-		#endif
-	}
-	*/
-
-	/*
-	/// Teste do Insertion Sort
-	cout << obj.analytics_insertionsort();
-	#ifdef VERBOSE
-		cout << "[depois do insertion]: ";
-		obj.printDados();
-		cout << endl;
-	#endif
-
-	/// Teste do Merge Sort
-	cout << obj.analytics_mergesort();
-	#ifdef VERBOSE
-		cout << "[depois do merge]: ";
-		obj.printDados();
-		cout << endl;
-	#endif
-	*/
-
-	/*XXX check cases
-	/// Teste do Quick Sort
-	cout << obj.analytics_quicksort();
-	#ifdef VERBOSE
-		cout << "[depois do quick]: ";
-		obj.printDados();
-		cout << endl;
-	#endif
-	*/
-
-	/*FIXME
-	/// Teste do Heap Sort
-	cout << obj.analytics_heapsort();
-	#ifdef VERBOSE
-		cout << "[depois do heap]: ";
-		obj.printDados();
-		cout << endl;
-	#endif
-	*/
-
-	/*
-	/// Teste do Bubble Sort
-	cout << obj.analytics_bubblesort();
-	#ifdef VERBOSE
-		cout << "[depois do bubble]: ";
-		obj.printDados();
-		cout << endl;
-	#endif
-	*/
-
-
-
 
 
 	return 0;
