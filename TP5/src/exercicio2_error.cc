ba55281eec06d15f56fb092f58baf2f70ed34a88
micalevisk <mllc@icomp.ufam.edu.br>
2017-05-16 21:51:06 -0400

    corrigir 6x6 (3,5)

--- TP5/src/exercicio2_error.cc
+++ TP5/src/exercicio2_error.cc
@@ -7,7 +7,9 @@
 #include <iostream>
 using namespace std;
 
-#define ORDEM_MATRIZ 6
+#ifndef ORDEM_MATRIZ
+	#define ORDEM_MATRIZ 6
+#endif
 
 #define OCORRENCIA_MAX ORDEM_MATRIZ/2
 #define QTD_LINHAS     ORDEM_MATRIZ
@@ -41,147 +43,6 @@ Matriz<short> tabuleiro = Matriz<short>(QTD_LINHAS, QTD_COLUNAS);
 
 
 
-/**
- * LINHA
- * buscar pares (células iguais consecutivos) na linha dada
- * e definir as células das laterais
- */
-void findPairsOnRow(unsigned i){
-	for(unsigned j=0; j < QTD_COLUNAS; ++j){
-		short* curr = tabuleiro.getElemento(i,j);
-		if(!curr || *curr == CELULA_INDEFINIDA) continue;
-
-		if( tabuleiro.celulasIguais(i,j , i,j+1) ){///PAR ENCONTRADO
-			short valorInverso = !(*curr);
-			tabuleiro.setElemento(i,j-1, valorInverso);
-			tabuleiro.setElemento(i,j+2, valorInverso);
-			j++;
-		}
-	}
-}
-
-/**
- * COLUNA
- * buscar pares (células iguais consecutivos) na coluna dada
- * e definir as células das laterais
- */
-void findPairsOnColumn(unsigned j){
-	for(unsigned i=0; i < QTD_LINHAS; ++i){
-		short* curr = tabuleiro.getElemento(i,j);
-		if(!curr || *curr == CELULA_INDEFINIDA) continue;
-
-		if( tabuleiro.celulasIguais(i,j , i+1,j) ){///PAR ENCONTRADO
-			short valorInverso = !(*curr);
-			tabuleiro.setElemento(i-1,j, valorInverso);
-			tabuleiro.setElemento(i+2,j, valorInverso);
-			i++;
-		}
-	}
-}
-
-
-/**
- * LINHA
- * buscar "trios", i.e., pares que estão "separados" por
- * uma célula vazia
- */
-void findTriosOnRow(unsigned i){
-	for(unsigned j=0; j < QTD_COLUNAS; ++j){
-		short* curr = tabuleiro.getElemento(i,j);
-		if(!curr || *curr == CELULA_INDEFINIDA) continue;
-
-		if( tabuleiro.celulasIguais(i,j , i,j+2) ){///TRIO ENCONTRADO (tipo X 2 X)
-			short valorInverso = !(*curr);
-			tabuleiro.setElemento(i,j+1, valorInverso);
-			j++;
-		}
-	}
-}
-
-/**
- * COLUNA
- * buscar "trios", i.e., pares que estão "separados" por
- * uma célula vazia
- */
-void findTriosOnColumn(unsigned j){
-	for(unsigned i=0; i < QTD_LINHAS; ++i){
-		short* curr = tabuleiro.getElemento(i,j);
-		if(!curr || *curr == CELULA_INDEFINIDA) continue;
-
-		if( tabuleiro.celulasIguais(i,j , i+2,j) ){///TRIO ENCONTRADO (tipo X 2 X)
-			short valorInverso = !(*curr);
-			tabuleiro.setElemento(i+1,j, valorInverso);
-			i++;
-		}
-	}
-}
-
-
-/**
- * LINHA
- * conta a quantidade de 0/1 e caso algum tenha 3 ocorrências
- * procurar as células vazias na linha dada e preencher com o seu inverso
- */
-void completeRow(unsigned i){
-	unsigned ocorrencias[2] = {0 , 0};///[0] é a quantidade de 0s, [1] a quantidade de 1s
-	///contar a quantidade de 0s e 1s na linha 'i'
-	for(unsigned j=0; j < QTD_COLUNAS; ++j){
-		short* curr = tabuleiro.getElemento(i,j);
-		if(!curr || *curr == CELULA_INDEFINIDA) continue;
-		ocorrencias[ *curr ]++;
-	}
-
-	for(short k=0; k < 2; ++k){
-		unsigned qtd = ocorrencias[k];
-		if(qtd == OCORRENCIA_MAX){
-			// cout << "MAXIMO DE " << k << " NA LINHA " << i << endl;
-			short valorInverso = !k;
-
-			///alterar células indefinidas (buscar por "2" nessa linha)
-			for(unsigned j=0; j < QTD_COLUNAS; ++j){
-				short* curr = tabuleiro.getElemento(i,j);
-				if(!curr) continue;///não deve ocorrer
-				if(*curr == CELULA_INDEFINIDA) tabuleiro.setElemento(i,j, valorInverso);
-			}
-
-			break;
-		}
-	}
-}
-
-/**
- * COLUNA
- * conta a quantidade de 0/1 e caso algum tenha 3 ocorrências
- * procurar as células vazias na coluna dada e preencher com o seu inverso
- */
-void completeColumn(unsigned j){
-	unsigned ocorrencias[2] = {0 , 0};///[0] é a quantidade de 0s, [1] a quantidade de 1s
-	///contar a quantidade de 0s e 1s na linha 'i'
-	for(unsigned i=0; i < QTD_LINHAS; ++i){
-		short* curr = tabuleiro.getElemento(i,j);
-		if(!curr || *curr == CELULA_INDEFINIDA) continue;
-		ocorrencias[ *curr ]++;
-	}
-
-	for(short k=0; k < 2; ++k){
-		unsigned qtd = ocorrencias[k];
-		if(qtd == OCORRENCIA_MAX){
-			// cout << "MAXIMO DE " << k << " NA LINHA " << i << endl;
-			short valorInverso = !k;
-
-			///alterar células indefinidas (buscar por "2" nessa linha)
-			for(unsigned i=0; i < QTD_LINHAS; ++i){
-				short* curr = tabuleiro.getElemento(i,j);
-				if(!curr) continue;///não deve ocorrer
-				if(*curr == CELULA_INDEFINIDA) tabuleiro.setElemento(i,j, valorInverso);
-			}
-
-			break;
-		}
-	}
-}
-
-
 /* REGRAS:
 {1} não deve ter 3 células consecutivas iguais
 {2} não deve ter mais que 3 ocorrências de 0s e 1s
@@ -193,7 +54,10 @@ bool regra1Valida_linha(unsigned i){
 		if(!curr || *curr == CELULA_INDEFINIDA) continue;
 
 		if( tabuleiro.celulasIguais(i,j , i,j+1) &&
-			tabuleiro.celulasIguais(i,j , i,j+2) ) return false;
+			tabuleiro.celulasIguais(i,j , i,j+2) ){
+				fprintf(stderr, "\tFALHOU NA REGRA 1 - LINHA\n");
+				return false;
+			}
 	}
 
 	return true;
@@ -204,7 +68,10 @@ bool regra1Valida_coluna(unsigned j){
 		short* curr = tabuleiro.getElemento(i,j);
 		if(!curr || *curr == CELULA_INDEFINIDA) continue;
 		if( tabuleiro.celulasIguais(i,j , i+1,j) &&
-			tabuleiro.celulasIguais(i,j , i+2,j) ) return false;
+			tabuleiro.celulasIguais(i,j , i+2,j) ){
+				fprintf(stderr, "\tFALHOU NA REGRA 1 - COLUNA\n");
+				return false;
+			}
 	}
 
 	return true;
@@ -221,7 +88,10 @@ bool regra2Valida_linha(unsigned i){
 		if(!curr || *curr == CELULA_INDEFINIDA) continue;
 
 		ocorrencias[ *curr ]++;
-		if( ocorrenciasInvalida(ocorrencias) ) return false;
+		if( ocorrenciasInvalida(ocorrencias) ){
+			fprintf(stderr, "\tFALHOU NA REGRA 2 - LINHA\n");
+			return false;
+		}
 	}
 
 	return true;
@@ -235,7 +105,10 @@ bool regra2Valida_coluna(unsigned j){
 		if(!curr || *curr == CELULA_INDEFINIDA) continue;
 
 		ocorrencias[ *curr ]++;
-		if( ocorrenciasInvalida(ocorrencias) ) return false;
+		if( ocorrenciasInvalida(ocorrencias) ){
+			fprintf(stderr, "\tFALHOU NA REGRA 2 - COLUNA\n");
+			return false;
+		}
 	}
 
 	return true;
@@ -254,6 +127,7 @@ bool regrasValidasPara(unsigned i, unsigned j){//admite coordenada (i,j) válida
 	if( !regra2Valida_linha(i) || !regra2Valida_coluna(j) ) return false;
 
 	///regra 3
+	//verifica se é última célula da linha e procura alguma parecida
 
 	return true;
 }
@@ -264,24 +138,31 @@ bool regrasValidasPara(unsigned i, unsigned j){//admite coordenada (i,j) válida
  * Se houver quebra de alguma regra, retornar false, senão true
  */
 bool freedomAtRow(unsigned i, unsigned j, short valor){
-	if(i == QTD_LINHAS) return true;
+	if(i == QTD_LINHAS) return true;///fim
 	if(j == QTD_COLUNAS) return freedomAtRow(i+1, 0, valor);//vai pra próxima linha
 
 	short* curr = tabuleiro.getElemento(i,j);
 	if(!curr) return true;///não deve ocorrer
 
-	if(*curr == CELULA_INDEFINIDA){
-		tabuleiro.setElemento(i,j, valor);
-		if( !regrasValidasPara(i,j) )
-			tabuleiro.setElemento(i,j, !valor);
-			if( !regrasValidasPara(i,j) ) return false;
+	if(*curr != CELULA_INDEFINIDA) return freedomAtRow(i,j+1, valor);
+	fprintf(stderr,"encontrou (%hd,%hd)\n", i,j);
+
+	tabuleiro.setElemento(i,j, valor);
+	fprintf(stderr,"\ttentando %hd \n", valor);
+
+	if( !regrasValidasPara(i,j) ){
+		fprintf(stderr,"\tfalhou pra %hd \n",valor);
+		tabuleiro.setElemento(i,j, !valor);
+		fprintf(stderr,"\ttentando %hd \n", !valor);
+		if( !regrasValidasPara(i,j) ){
+			fprintf(stderr,"\t\tfalhou pra %hd \n",!valor);
+			return false;//FIXME para quando tenta a segunda vez e nao tenta anterior
+		}
 	}
 
+	fprintf(stderr,"\tchamando prox val = %hd \n", valor);
 	return freedomAtRow(i,j+1, valor);
 }
-
-
-
 
 
 
@@ -290,21 +171,7 @@ main()
 {
 	tabuleiro.lerValoresFormatados();
 
-
-	{///PROCESSO QUE PREENCHE ALGUMAS CÉLULAS USANDO MACETES VÁLIDOS
-		for(unsigned i=0; i < QTD_LINHAS; ++i)  findPairsOnRow(i);
-		for(unsigned j=0; j < QTD_COLUNAS; ++j) findPairsOnColumn(j);
-
-		for(unsigned i=0; i < QTD_LINHAS; ++i)  findTriosOnRow(i);
-		for(unsigned j=0; j < QTD_COLUNAS; ++j) findTriosOnColumn(j);
-
-		for(unsigned i=0; i < QTD_LINHAS; ++i)  completeRow(i);
-		for(unsigned j=0; j < QTD_COLUNAS; ++j) completeColumn(j);
-	}
-
-
 	freedomAtRow(0,0, 0);
-
 
 	tabuleiro.imprimirValores();
 }
