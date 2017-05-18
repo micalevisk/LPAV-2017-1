b9d40f3ee30d01df6c31961c6ffe31840de2ce48
micalevisk <mllc@icomp.ufam.edu.br>
2017-05-18 18:52:51 -0400

    add uma condicional

--- TP5/src/_testes/runall.sh
+++ TP5/src/_testes/runall.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 g++ -std=c++11 -D ORDEM_MATRIZ=6  ../exercicio2.cc -o main
+[[ $? -eq 0 ]] && {
 
-for i in entrada*
-do
-	[[ $i =~ [0-9]$ ]] && {
-		echo "==== TESTANDO $i ===="
-		diff -w <(tail -n6 $i) <(./main < $i 2> /dev/null)
-	}
-done
+	for i in entrada*
+	do
+		[[ $i =~ [0-9]$ ]] && {
+			echo "==== TESTANDO $i ===="
+			diff -w <(tail -n6 $i) <(./main < $i 2> /dev/null)
+		}
+	done
 
-rm main*
+	rm main*
+}
