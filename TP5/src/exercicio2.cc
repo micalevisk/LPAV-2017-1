46df4f72e002e70d41b705cef05c6b83ca4e9dbd
micalevisk <mllc@icomp.ufam.edu.br>
2017-05-18 18:27:40 -0400

    solução pronta (não refatorada)

--- TP5/src/exercicio2.cc
+++ TP5/src/exercicio2.cc
@@ -5,6 +5,7 @@
  */
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #ifndef ORDEM_MATRIZ
@@ -36,6 +37,8 @@ template<class T> class Matriz {
 		void setElemento(unsigned i, unsigned j, const T newValue);
 		T* getElemento(unsigned i, unsigned j);
 		bool celulasIguais(unsigned ai, unsigned aj, unsigned bi, unsigned bj);
+		vector<unsigned> getLinhasComecamCom(T x, unsigned exceto);
+		vector<unsigned> getColunasComecamCom(T x, unsigned exceto);
 };
 
 Matriz<short> tabuleiro = Matriz<short>(QTD_LINHAS, QTD_COLUNAS);
@@ -43,11 +46,7 @@ Matriz<short> tabuleiro = Matriz<short>(QTD_LINHAS, QTD_COLUNAS);
 
 
 
-/* REGRAS:
-{1} não deve ter 3 células consecutivas iguais
-{2} não deve ter mais que 3 ocorrências de 0s e 1s
-{3} linhas e colunas únicas
-*/
+// {1} não deve ter 3 células consecutivas iguais
 bool regra1Valida_linha(unsigned i){
 	for(unsigned j=0; j < QTD_COLUNAS; ++j){
 		short* curr = tabuleiro.getElemento(i,j);
@@ -62,7 +61,6 @@ bool regra1Valida_linha(unsigned i){
 
 	return true;
 }
-
 bool regra1Valida_coluna(unsigned j){
 	for(unsigned i=0; i < QTD_LINHAS; ++i){
 		short* curr = tabuleiro.getElemento(i,j);
@@ -78,8 +76,12 @@ bool regra1Valida_coluna(unsigned j){
 }
 
 
-bool ocorrenciasInvalida(unsigned* ocorrencias){ return ocorrencias[0] > OCORRENCIA_MAX || ocorrencias[1] > OCORRENCIA_MAX; }
+bool ocorrenciasInvalida(unsigned* ocorrencias){
+	return ocorrencias[0] > OCORRENCIA_MAX
+	|| ocorrencias[1] > OCORRENCIA_MAX;
+}
 
+// {2} não deve ter mais que 3 ocorrências de 0s e 1s
 bool regra2Valida_linha(unsigned i){
 	unsigned ocorrencias[2] = {0,0};
 
@@ -96,7 +98,6 @@ bool regra2Valida_linha(unsigned i){
 
 	return true;
 }
-
 bool regra2Valida_coluna(unsigned j){
 	unsigned ocorrencias[2] = {0,0};
 
@@ -115,6 +116,66 @@ bool regra2Valida_coluna(unsigned j){
 }
 
 
+bool linhaCompleta(unsigned i){
+	for(unsigned j=0; j < QTD_COLUNAS; ++j){
+		short* curr = tabuleiro.getElemento(i,j);
+		if(!curr) continue;
+		if(*curr == CELULA_INDEFINIDA) return false;
+	}
+	return true;
+}
+bool colunaCompleta(unsigned j){
+	for(unsigned i=0; i < QTD_LINHAS; ++i){
+		short* curr = tabuleiro.getElemento(i,j);
+		if(!curr) continue;
+		if(*curr == CELULA_INDEFINIDA) return false;
+	}
+	return true;
+}
+
+
+bool linhaRepetidaComInicial(short valor, unsigned i){
+	vector<unsigned> linhas = tabuleiro.getLinhasComecamCom(valor,i);
+	for(const unsigned linha : linhas){
+		unsigned j=0;
+		do{
+			if( !tabuleiro.celulasIguais(i,j , linha,j) ) break;
+			++j;
+		}while(j < QTD_COLUNAS);
+		if(j==QTD_COLUNAS) return true;
+	}
+	return false;
+}
+bool colunaRepetidaComInicial(short valor, unsigned j){
+	vector<unsigned> colunas = tabuleiro.getLinhasComecamCom(valor,j);
+	for(const unsigned coluna : colunas){
+		unsigned i=0;
+		do{
+			if( !tabuleiro.celulasIguais(i,j , i,coluna) ) break;
+			++i;
+		}while(i < QTD_LINHAS);
+		if(i==QTD_LINHAS) return true;
+	}
+	return false;
+}
+
+// {3} linhas e colunas únicas
+bool regra3Valida_linha(unsigned i){
+	if( !linhaCompleta(i) ) return true;
+	if( linhaRepetidaComInicial(0,i) || linhaRepetidaComInicial(1,i) ){
+		// fprintf(stderr, "\tFALHOU NA REGRA 3 - LINHA\n");
+		return false;
+	}
+	return true;
+}
+bool regra3Valida_coluna(unsigned j){
+	if( !colunaCompleta(j) ) return true;
+	if( colunaRepetidaComInicial(0,j) || colunaRepetidaComInicial(1,j) ){
+		// fprintf(stderr, "\tFALHOU NA REGRA 3 - COLUNA\n");
+		return false;
+	}
+	return true;
+}
 
 
 bool regrasValidasPara(unsigned i, unsigned j){//admite coordenada (i,j) válida
@@ -127,34 +188,34 @@ bool regrasValidasPara(unsigned i, unsigned j){//admite coordenada (i,j) válida
 	if( !regra2Valida_linha(i) || !regra2Valida_coluna(j) ) return false;
 
 	///regra 3
-	//verifica se é última célula da linha e procura alguma parecida
+	if( !regra3Valida_linha(i) || !regra3Valida_coluna(j) ) return false;
 
 	return true;
 }
 
 
-bool freedomAtRow(unsigned i, unsigned j, short valor){
+bool freedomAtRow(unsigned i, unsigned j){
 	if(i == QTD_LINHAS) return true;///fim
-	if(j == QTD_COLUNAS) return freedomAtRow(i+1, 0, valor);//vai pra próxima linha
+	if(j == QTD_COLUNAS) return freedomAtRow(i+1, 0);//vai pra próxima linha
 
 	short* curr = tabuleiro.getElemento(i,j);
 	if(!curr) return true;///não deve ocorrer
 
-	if(*curr != CELULA_INDEFINIDA) return freedomAtRow(i,j+1, valor);
+	if(*curr != CELULA_INDEFINIDA) return freedomAtRow(i,j+1);
 	// fprintf(stderr,"encontrou (%hd,%hd)\n", i,j);
 
 	tabuleiro.setElemento(i,j, 0);
 	// fprintf(stderr,"\ttentando %hd \n", 0);
 
-	if( regrasValidasPara(i,j) )
-		if( freedomAtRow(i,j+1, 0) ) return true;
+	if( regrasValidasPara(i,j) )//*
+		if( freedomAtRow(i,j+1) ) return true;
 
 	///falhou com 0, tenta com 1
 	tabuleiro.setElemento(i,j, 1);
 	// fprintf(stderr,"\ttentando %hd \n", 1);
 
-	if( regrasValidasPara(i,j) )
-		if( freedomAtRow(i,j+1, 1) ) return true;
+	if( regrasValidasPara(i,j) )//*
+		if( freedomAtRow(i,j+1) ) return true;
 
 	///falhou com 1 ou com algum caso do próximo
 	tabuleiro.setElemento(i,j, CELULA_INDEFINIDA);
@@ -169,7 +230,7 @@ main()
 {
 	tabuleiro.lerValoresFormatados();
 
-	freedomAtRow(0,0, 0);
+	freedomAtRow(0,0);
 
 	tabuleiro.imprimirValores();
 }
@@ -222,4 +283,24 @@ template<class T> bool Matriz<T>::celulasIguais(unsigned ai, unsigned aj, unsign
 	T* A = getElemento(ai,aj);
 	T* B = getElemento(bi,bj);
 	return (A && B) && (*A == *B);
+}
+
+template<class T> vector<unsigned> Matriz<T>::getLinhasComecamCom(T x, unsigned exceto){
+	vector<unsigned> linhas;
+	for(unsigned i=0; i < qtdLinhas; ++i){
+		T* curr = getElemento(i,0);
+		if(!curr || i==exceto) continue;
+		if(*curr == x) linhas.push_back(i);
+	}
+	return linhas;
+}
+
+template<class T> vector<unsigned> Matriz<T>::getColunasComecamCom(T x, unsigned exceto){
+	vector<unsigned> colunas;
+	for(unsigned j=0; j < qtdColunas; ++j){
+		T* curr = getElemento(0,j);
+		if(!curr || j==exceto) continue;
+		if(*curr == x) colunas.push_back(j);
+	}
+	return colunas;
 }
