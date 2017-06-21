9703b2b2401a5303be43bdd712b1bda5816aad34
Micael Levi <mllc@icomp.ufam.edu.br>
2017-06-21 18:52:55 -0400

    add assert para a quantidade de threads

--- TP10/exercicio 1/exercicio1.cpp
+++ TP10/exercicio 1/exercicio1.cpp
@@ -47,7 +47,7 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	int qtdThreads = atoi(argv[1]);
+	int qtdThreads = atoi(argv[1]); assert(qtdThreads >= 0);
 	int qtdElementosLidos;
 	unsigned qtdElementosParaCada;
 	pthread_t* arrThreads;
