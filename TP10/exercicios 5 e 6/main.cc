a436297639ddedd6c7e3aded088c5d22ff8fe501
Micael Levi <mllc@icomp.ufam.edu.br>
2017-06-21 21:44:57 -0400

    try

--- TP10/exercicios 5 e 6/main.cc
+++ TP10/exercicios 5 e 6/main.cc
@@ -2,8 +2,8 @@
 //	[main.cc]
 //	Created by Micael Levi on 06/19/2017
 //	Copyright (c) 2017 mllc@icomp.ufam.edu.br; All rights reserved.
-//	g++ -std=c++11 -O2 -o testes/main -I../lib ordenacaoAnalytics.cc main.cc algoritmos/quicksort_sequencial.cc -D DEBUG -D VERBOSE -D MILLISECONDS
 //
+//	g++ -std=c++11 -o main -I../lib ordenacaoAnalytics.cc main.cc algoritmos/quicksort_sequencial.cpp  algoritmos/quicksort_paralelo.cpp -lpthread -D DEBUG -D VERBOSE -D MILLISECONDS
 
 #include <iostream>
 #include <cstdio>
@@ -11,20 +11,26 @@ using namespace std;
 
 #include "ordenacaoAnalytics.hpp"
 
-
+//TODO adicionar opção de escolha de algoritmo
 int main(int argc, char* argv[])
 {
-	if(argc != 2) return 1;
+	if(argc != 2){
+		fprintf(stdout, "USAGE: ");
+		fprintf(stdout, "%s <nome do arquivo com as entradas>\n", argv[0]);
+		return 1;
+	}
+
 	const char* nomeArquivo = argv[1];
 
-	OrdenacaoAnalytics obj(nomeArquivo);
+	OrdenacaoAnalytics analise(nomeArquivo);
 
 	#ifdef VERBOSE
 		cout << "ORIGINAL:";
-		obj.printFileData(',');
+		analise.printFileData(',');
 	#endif
 
-	cout << obj.getAnalytics_quicksort_sequencial();
+	// cout << analise.getAnalytics_quicksort_sequencial();
+	cout << analise.getAnalytics_quicksort_paralelo();
 
 	return 0;
 }
