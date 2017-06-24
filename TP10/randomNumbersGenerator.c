206cc5d6e70ee0441b15d55113c1c27f04da9254
Micael Levi <mllc@icomp.ufam.edu.br>
2017-06-23 21:15:58 -0400

    add opção para valor máximo & fix output

--- TP10/randomNumbersGenerator.c
+++ TP10/randomNumbersGenerator.c
@@ -1,7 +1,14 @@
+//
+//	[randomNumbersGenerator]
+//	Created by Micael Levi on 06/21/2017
+//	Copyright (c) 2017 mllc@icomp.ufam.edu.br; All rights reserved.
+//
+
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
+#include <limits.h>
 
 int main(int argc, char* argv[])
 {
@@ -11,29 +18,32 @@ int main(int argc, char* argv[])
 
 	srand(time(NULL));
 
-	if(argc != 3){
+	if(argc < 3){
 		fprintf(stdout, "USAGE: ");
-		fprintf(stdout, "%s <output_text_file> <amount>\n", argv[0]);
+		fprintf(stdout, "%s <output_text_file> <amount> [max_value]\n", argv[0]);
 		return 1;
 	}
 
 	filename = argv[1];
 	valuesAmount = atoi(argv[2]);
+	if(valuesAmount < 0) return 2;
 
 	outputFile = fopen(filename, "w");
 	if(!outputFile){
 		perror("ERROR: On Create File");
-		return 2;
+		return 3;
 	}
 
-	fprintf(outputFile, "%d\n", valuesAmount);
-	fprintf(stdout, "%d\n", valuesAmount);
+	fprintf(outputFile, "%u\n", valuesAmount);
+	fprintf(stdout, "%u\n", valuesAmount);
 
+	unsigned long long int maxValue = ULLONG_MAX - 1;
+	if(argc == 4) maxValue = strtoull(argv[3], (char **)NULL, 10);
 	///Gera, insere e imprime os N números aleatórios
 	for(unsigned i=0; i < valuesAmount; ++i){
-		int randomNumber = rand();
-		fprintf(outputFile, "%d\n", randomNumber);
-		fprintf(stdout, "%d\n", randomNumber);
+		unsigned long long int randomNumber = rand() % (maxValue+1);
+		fprintf(outputFile, "%llu\n", randomNumber);
+		fprintf(stdout, "%llu\n", randomNumber);
 	}
 
 	fclose(outputFile);
