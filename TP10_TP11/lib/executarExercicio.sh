7b387353575f88e7d94c60bbc83bba0ad7e47414
micalevisk <mllc@icomp.ufam.edu.br>
2017-06-28 19:05:14 -0400

    add script para executar exercicios 1 a 4

---
+++ TP10_TP11/lib/executarExercicio.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+
+
+# -------------------------- [ CONFIGURAÇÕES ] -------------------------- #
+# declare -ri MAIOR_NUMERO=1000000
+# declare -a INSTANCIAS=("32k" "64k" "128k" "256k" "512k" "1024k" "2048k")
+declare -a INSTANCIAS=("32k")
+declare -a THREADS=(4 16 32 64 128 256 512 1024 2048 4096)
+declare -i numExercicio
+
+readonly PATH_INPUT="../__dados__"
+# ----------------------------------------------------------------------- #
+
+
+[ -d "$PATH_INPUT" ] || {
+	imprimir "Erro ao procurar por '$PATH_INPUT'"
+	exit 2
+}
+
+function imprimir(){
+	echo "$1" 1>&3
+}
+
+function die(){
+	imprimir "Não deve -run e -delete ao mesmo tempo"
+	exit 6
+}
+
+
+function compilar(){
+	imprimir "Compilando '$PATH_CODE'"
+	g++ -std=c++11 -I./ "$PATH_CODE" -o "$PATH_EXEC" -lpthread -D MILLISECONDS
+	[ ! $? ] && exit 3
+}
+
+function apagarArquivosGerados(){
+	imprimir "Apagando arquivos gerados"
+	rm -f "$PATH_EXEC"
+	rm -rf "$PATH_OUTPUT"
+}
+
+function executarExercicio(){
+	[[ -x $PATH_EXEC  ]] || compilar
+	[[ -d "$PATH_OUTPUT" ]] || mkdir -p "$PATH_OUTPUT"
+
+	for N in "${INSTANCIAS[@]}"
+	do
+		instancia="${N//k/000}"
+		caminhoSaidaInstancia="$PATH_OUTPUT/${N}"
+		entradaInstancia="$PATH_INPUT/${N}.txt"
+
+		[ -r "$entradaInstancia" ] || {
+			imprimir "Arquivo '$entradaInstancia' não encontrado/legível!"
+			exit 5
+		}
+
+		mkdir -p "$caminhoSaidaInstancia"
+
+		imprimir "Executar para instância $N"
+		for nthreads in "${THREADS[@]}"
+		do
+			saidaExec="$caminhoSaidaInstancia/${nthreads}_threads.txt"
+			sleep 1
+			imprimir "Executando $N com $nthreads threads"
+			( ./"$PATH_EXEC" $nthreads < $entradaInstancia ) 2>> "$saidaExec"
+		done
+	done
+}
+
+
+# ----------------------------------------------------------- #
+exec 3>/dev/null # sem verbose
+
+realizarExecucao=false
+realizarRemocao=false
+declare -i qtdExecucoes=1
+
+while getopts :ve:r:d opt
+do	case $opt in
+	v)	exec 3>&2 ;;
+	e)	numExercicio=$OPTARG ;;
+	r)
+		$realizarRemocao && die
+		realizarExecucao=true
+		qtdExecucoes=$OPTARG
+		;;
+
+	d)
+		$realizarExecucao && die
+		realizarRemocao=true
+		;;
+	\?) echo -e "USO: \e[40;36m$0\e[m \e[40;33m[-v]\e[40;33;1m -e<EXERCÍCIO> < -r<qtdExecucoes> , -d > \e[m"; exit 1;;
+	esac
+done
+
+[[ $numExercicio -gt 0 ]] || exit 4
+PATH_EXERCICIO="../exercicio $numExercicio"
+PATH_OUTPUT="${PATH_EXERCICIO}/__resultados__"
+PATH_CODE="${PATH_EXERCICIO}/exercicio${numExercicio}.cpp"
+PATH_EXEC="${PATH_CODE/.cpp}"
+
+$realizarRemocao && apagarArquivosGerados
+$realizarExecucao && {
+	i=1
+	while [[ $i -le qtdExecucoes ]]
+	do
+		imprimir "Execução $i"
+		executarExercicio
+		(( i++ ))
+	done
+}
+# ----------------------------------------------------------- #
